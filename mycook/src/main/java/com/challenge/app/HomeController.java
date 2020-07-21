5db4528f09b6b70981f4f822abd9f105ef074cb4
hong <hong@401-012>
2020-07-21 14:59:21 +0900

    comment 1

--- mycook/src/main/java/com/challenge/app/HomeController.java
+++ mycook/src/main/java/com/challenge/app/HomeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+	// comment 1
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
